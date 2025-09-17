f89d3689701d3d425a1e8034163b32ae7dc58aa3
Jonathan MFR Chris <a27182818284590e@gmail.com>
2025-09-17 09:50:58 -0600

    update

---
+++ Tempermonkey Script/Template.js
@@ -0,0 +1,15 @@
+// ==UserScript==
+// @name         New Userscript
+// @namespace    http://tampermonkey.net/
+// @version      2025-09-17
+// @description  try to take over the world!
+// @author       You
+// @match        https://*.example.com/*
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    // Your code here...
+})();
---
+++ Tempermonkey Script/TestScript.js
@@ -0,0 +1,15 @@
+// ==UserScript==
+// @name         Test Script
+// @namespace    http://tampermonkey.net/
+// @version      2025-09-17
+// @description  Testing Tempermonkey script from GitHub
+// @author       You
+// @match        *://*/*
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    console.log("Hello World! I am from GitHub!");
+})();
