7bb55a0a906074c55a45474fdd2b62c4b9b45a1e
Jonathan MFR Chris <a27182818284590e@gmail.com>
2025-09-18 19:43:26 -0600

    update

--- Tampermonkey Script/TestScript.js
+++ Tampermonkey Script/TestScript.js
@@ -11,6 +11,6 @@
 (function() {
     'use strict';
 
-    // console.log("Hello World! I am from GitHub!");
-    console.log("Testing2!");
+    console.log("Hello World! I am from GitHub!");
+    // console.log("Testing2!");
 })();
