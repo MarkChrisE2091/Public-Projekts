c352563783c42f33270db488c804311c65375d6c
Jonathan MFR Chris <a27182818284590e@gmail.com>
2025-09-18 19:53:10 -0600

    update

---
+++ Tampermonkey Script/RemoteScripts/Test Script.js
@@ -0,0 +1,14 @@
+// ==UserScript==
+// @name         Test Script
+// @namespace    http://tampermonkey.net/
+// @version      1.0
+// @description  Description here
+// @author       You
+// @match        *://*/*
+// @require      https://raw.githubusercontent.com/MarkChrisE2091/Public-Projekts/refs/heads/main/Tampermonkey%20Script/TestScript.js
+// @updateURL    https://raw.githubusercontent.com/MarkChrisE2091/Public-Projekts/refs/heads/main/Tampermonkey%20Script/TestScript.js
+// @downloadURL  https://raw.githubusercontent.com/MarkChrisE2091/Public-Projekts/refs/heads/main/Tampermonkey%20Script/TestScript.js
+// @grant        none
+// ==/UserScript==
+
+// The required script will run automatically
