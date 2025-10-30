ed354b667d640ccc44f691dd918a4a0c2852dd45
Jonathan MFR Chris <a27182818284590e@gmail.com>
2025-10-29 20:16:51 -0600

    update

--- Tampermonkey Script/Perplexity Usage.js
+++ Tampermonkey Script/Perplexity Usage.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Perplexity Usage
 // @namespace    http://tampermonkey.net/
-// @version      1.2
+// @version      1.3
 // @description  Perplexity Usage
 // @author       You
 // @match        https://*.perplexity.ai/*
