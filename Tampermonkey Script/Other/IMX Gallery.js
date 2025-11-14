0754d8d5bccd69eb783badade5ad3eb500e922ae
Jonathan MFR Chris <a27182818284590e@gmail.com>
2025-11-13 22:32:56 -0700

    update

---
+++ Tampermonkey Script/Other/IMX Gallery.js
@@ -0,0 +1,27 @@
+// ==UserScript==
+// @name         IMX Gallery
+// @namespace    Browser Scripts
+// @version      1.0
+// @description  List of actions for IMX Gallery
+// @author       You
+// @match        https://*.imx.to/*
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    function passRobotTest(){
+        let button = document.querySelector('input[type="submit"]');
+        if (button){
+            button.click();         // Found it! Click and stop
+            console.log("Button Click!")
+        }
+        else{
+            console.log("button doesn't exists. Retry again in a few second");
+            setTimeout(passRobotTest, 500);  // Not found, try again in 500ms
+        }
+    }
+
+    passRobotTest();
+})();
