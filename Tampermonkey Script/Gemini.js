bd0b4877e7c6b3720af042398003a3a6fb39cdea
Jonathan MFR Chris <a27182818284590e@gmail.com>
2025-12-26 23:17:51 -0700

    update

--- Tampermonkey Script/Gemini.js
+++ Tampermonkey Script/Gemini.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Gemini Usage
 // @namespace    Browser Scripts
-// @version      1.2
+// @version      1.3
 // @description  Gemini Usage
 // @author       You
 // @match        https://gemini.google.com/*
